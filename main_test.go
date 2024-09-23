e453a992962dccc222ea5061e9e66f7d9af2f922
000xE <41651226+000xE@users.noreply.github.com>
2024-09-23 23:09:53 +0100

    Download FBD data for each country for each league

---
+++ main_test.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"os"
+	"testing"
+)
+
+func TestCountries(t *testing.T) {
+	for country, code := range Countries {
+		_, exists := Leagues[code]
+		if !exists {
+			t.Errorf("%v does not exist with a league", country)
+		}
+	}
+}
+
+func TestGetEnvVariables(t *testing.T) {
+	os.Setenv("password", "test")
+	os.Setenv("years", "abc")
+
+	_, _, _, err := GetEnvVariables()
+	if err == nil {
+		t.Error("Email is supposed to be invalid")
+	}
+
+	os.Setenv("email", "test@gmail.com")
+	os.Setenv("years", "abc")
+
+	_, _, _, err = GetEnvVariables()
+	if err == nil {
+		t.Error("Password is supposed to be invalid")
+	}
+
+	os.Setenv("email", "test@gmail.com")
+	os.Setenv("password", "test")
+
+	_, _, _, err = GetEnvVariables()
+	if err == nil {
+		t.Error("Year is supposed to be invalid")
+	}
+
+	os.Setenv("email", "test@gmail.com")
+	os.Setenv("password", "test")
+	os.Setenv("years", "abc")
+
+	_, _, _, err = GetEnvVariables()
+	if err == nil {
+		t.Error("Year is supposed to be invalid")
+	}
+
+	os.Setenv("email", "test@gmail.com")
+	os.Setenv("password", "test")
+	os.Setenv("years", "1")
+
+	_, _, _, err = GetEnvVariables()
+	if err != nil {
+		t.Error("All environment variables are supposed to be valid")
+	}
+}
